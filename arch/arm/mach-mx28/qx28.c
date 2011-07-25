b734fede5112e9f31c91c02bf07f0d52126a556d
Michael Heimpold <mhe@iequalize.de>
2011-07-25 16:28:55 +0200

    Extend support for QX28 module
    - Support for GPIO buttons on LON28 addon module
    - Support for GPIO leds on LON28 addon module
    - Support for AT25 EEPROM on LON28 addon module
    - Various whitespace/naming fixes
    
    Signed-off-by: Michael Heimpold <mhe@iequalize.de>

--- arch/arm/mach-mx28/qx28.c
+++ arch/arm/mach-mx28/qx28.c
@@ -36,89 +36,194 @@
 #include <mach/pinctrl.h>
 
 #include <linux/spi/spi.h>
-#include <linux/spi/flash.h>
+#include <linux/spi/eeprom.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
 #include <linux/mtd/physmap.h>
+
+#include <linux/gpio-keys.h>
+#include <linux/leds.h>
 
 #include "device.h"
 #include "module-ieqma28.h"
 #include "qx28.h"
 
-
-#if defined(CONFIG_GPIO_PCA953X) || defined(CONFIG_GPIO_PCA953X_MODULE)
-static struct pca953x_platform_data qx28_pca953x_pdata = {
-	.gpio_base = -1,
-};
+#ifndef NR_BUILTIN_GPIO
+#define NR_BUILTIN_GPIO 160
 #endif
 
-#if defined(CONFIG_LEDS_PCA955X) || defined(CONFIG_LEDS_PCA955X_MODULE)
+#define QX28_LON28_GPIO(chip, pin) (NR_BUILTIN_GPIO + chip * 8 + pin)
 
-#define DEFINE_LED(id, chip, color, trigger) \
-               [id] = { .name = chip ":" color ":led" #id, .default_trigger = trigger }
-
-static struct led_info qx28_pca955x_leds[] = {
-	DEFINE_LED(0, "ic201", "red",    "none"),
-	DEFINE_LED(1, "ic201", "yellow", "none"),
-	DEFINE_LED(2, "ic201", "green",  "none"),
-	DEFINE_LED(3, "ic201", "yellow", "none"),
-	DEFINE_LED(4, "ic201", "yellow", "none"),
-	DEFINE_LED(5, "ic201", "yellow", "none"),
-	DEFINE_LED(6, "ic201", "yellow", "none"),
-	DEFINE_LED(7, "ic201", "yellow", "none"),
+#if defined(CONFIG_GPIO_PCA953X) || defined(CONFIG_GPIO_PCA953X_MODULE)
+static struct pca953x_platform_data qx28_lon28_pca953x_1_pdata = {
+	.gpio_base = QX28_LON28_GPIO(0, 0),
 };
 
-static struct led_platform_data qx28_pca955x_leds_pdata = {
-	.num_leds = ARRAY_SIZE(qx28_pca955x_leds),
-	.leds = qx28_pca955x_leds,
+static struct pca953x_platform_data qx28_lon28_pca953x_2_pdata = {
+	.gpio_base = QX28_LON28_GPIO(1, 0),
+};
+
+static struct pca953x_platform_data qx28_lon28_pca953x_3_pdata = {
+	.gpio_base = QX28_LON28_GPIO(2, 0),
 };
 #endif
 
 static struct i2c_board_info __initdata qx28_i2c_devices[] = {
 #if defined(CONFIG_GPIO_PCA953X) || defined(CONFIG_GPIO_PCA953X_MODULE)
+	/* LON28 */
 	{
 		I2C_BOARD_INFO("pca9534", 0x20),
-		.platform_data = &qx28_pca953x_pdata,
+		.platform_data = &qx28_lon28_pca953x_1_pdata,
 // TODO: .irq
+	},
+	{
+		I2C_BOARD_INFO("pca9534", 0x21),
+		.platform_data = &qx28_lon28_pca953x_2_pdata,
+	},
+	{
+		I2C_BOARD_INFO("pca9534", 0x22),
+		.platform_data = &qx28_lon28_pca953x_3_pdata,
 	},
 #endif
 #if defined(CONFIG_RTC_DRV_PCF2127A) || defined(CONFIG_RTC_DRV_PCF2127A_MODULE)
+	/* TB96 (aka TERMINAL1) */
 	{
 		I2C_BOARD_INFO("pcf2127a", 0x51),
 	},
 #endif
-#if defined(CONFIG_LEDS_PCA955X) || defined(CONFIG_LEDS_PCA955X_MODULE)
+#if defined(CONFIG_RTC_DRV_DS1307) || defined(CONFIG_RTC_DRV_DS1307_MODULE)
+	/* QX96_ETH_USB_RTC */
 	{
-		I2C_BOARD_INFO("pca9551", 0x60),
-		.platform_data = &qx28_pca955x_leds_pdata,
+		I2C_BOARD_INFO("ds1340"), 0x68),
 	},
 #endif
 };
 
-static void __init i2c_device_init(void)
+static void __init qx28_i2c_init(void)
 {
 	i2c_register_board_info(0, qx28_i2c_devices, ARRAY_SIZE(qx28_i2c_devices));
 }
 
-#if defined(CONFIG_MTD_M25P80) || defined(CONFIG_MTD_M25P80_MODULE)
-static struct flash_platform_data mx28_spi_flash_data = {
-	.name = "m25p80",
-	.type = "w25x80",
+#if defined(CONFIG_KEYBOARD_GPIO) || defined(CONFIG_KEYBOARD_GPIO_MODULE)
+
+static struct gpio_keys_button qx28_lon28_gpio_keys[] = {
+	{
+		.type = EV_KEY,
+		.code = KEY_F1,
+		.gpio = QX28_LON28_GPIO(1, 0),
+		.desc = "Button A",
+		.active_low = 1,
+	}, {
+		.type = EV_KEY,
+		.code = KEY_F2,
+		.gpio = QX28_LON28_GPIO(1, 1),
+		.desc = "Button B",
+		.active_low = 1,
+	}, {
+		.type = EV_KEY,
+		.code = KEY_F3,
+		.gpio = QX28_LON28_GPIO(1, 2),
+		.desc = "LON Service",
+		.active_low = 1,
+	}, {
+		.type = EV_KEY,
+		.code = KEY_F5,
+		.gpio = QX28_LON28_GPIO(1, 3),
+		.desc = "LON SVC", /* FT5000 LON SVC */
+		.active_low = 1,
+	}, {
+		.type = EV_SW,
+		.code = SW_DOCK,
+		.gpio = QX28_LON28_GPIO(1, 4),
+		.desc = "LON Termination", /* LON 150 Ohm Termination Switch */
+		.active_low = 1,
+	},
+};
+
+static struct gpio_keys_platform_data qx28_lon28_gpio_keys_platform_data = {
+	.buttons = qx28_lon28_gpio_keys,
+	.nbuttons = ARRAY_SIZE(qx28_lon28_gpio_keys),
+	.rep = 0,
+};
+
+static struct platform_device qx28_lon28_gpio_keys_device = {
+	.name = "gpio-keys",
+	.id = -1,
+	.dev = {
+		.platform_data = &qx28_lon28_gpio_keys_platform_data,
+	},
+};
+
+static void __init qx28_lon28_keys_init(void)
+{
+	platform_device_register(&qx28_lon28_gpio_keys_device);
+}
+#else
+static inline void qx28_lon28_keys_init(void) {}
+#endif
+
+#if defined(CONFIG_LEDS_GPIO) || defined(CONFIG_LEDS_GPIO_MODULE)
+
+#define DEFINE_LED(pin, color, func) \
+               { .gpio = QX28_LON28_GPIO(0, pin), \
+                 .name = "qx28-lon28:" color ":" func, \
+                 .default_trigger = "none", .active_low = 1 }
+
+static struct gpio_led qx28_lon28_leds[] = {
+	DEFINE_LED(0, "green",  "led1"),
+	DEFINE_LED(1, "green",  "led3_1"),
+	DEFINE_LED(2, "yellow", "led3_2"),
+	DEFINE_LED(3, "green",  "led4_1"),
+	DEFINE_LED(4, "red",    "led4_2"),
+	DEFINE_LED(5, "green",  "led5"),
+};
+
+static struct gpio_led_platform_data qx28_lon28_led_pdata = {
+	.num_leds = ARRAY_SIZE(qx28_lon28_leds),
+	.leds = qx28_lon28_leds,
+};
+
+static struct platform_device qx28_lon28_led_device = {
+	.name = "leds-gpio",
+	.id = -1,
+	.dev = {
+		.platform_data = &qx28_lon28_led_pdata,
+	},
+};
+
+static void __init qx28_lon28_init_led(void)
+{
+	platform_device_register(&qx28_lon28_led_device);
+}
+#else
+static inline void qx28_lon28_init_led(void) {}
+#endif
+
+#if defined(CONFIG_EEPROM_AT25) || defined(CONFIG_EEPROM_AT25_MODULE)
+static struct spi_eeprom qx28_lon28_at25_pdata = {
+	.byte_len = SZ_64K / 8,
+	.name = "at25640",
+	.page_size = 32,
+	.flags = EE_ADDR2,
 };
 #endif
 
-static struct spi_board_info spi_board_info[] __initdata = {
-#if defined(CONFIG_MTD_M25P80) || defined(CONFIG_MTD_M25P80_MODULE)
+static struct spi_board_info qx28_spi_board_info[] __initdata = {
+#if defined(CONFIG_EEPROM_AT25) || defined(CONFIG_EEPROM_AT25_MODULE)
 	{
-		/* the modalias must be the same as spi device driver name */
-		.modalias = "m25p80", /* Name of spi_driver for this device */
-		.max_speed_hz = 20000000,     /* max spi clock (SCK) speed in HZ */
-		.bus_num = 1, /* Framework bus number */
-		.chip_select = 0, /* Framework chip select. */
-		.platform_data = &mx28_spi_flash_data,
+		.modalias = "at25",
+		.max_speed_hz = 5000000,
+		.bus_num = 0,
+		.chip_select = 0,
+		.platform_data = &qx28_lon28_at25_pdata,
 	},
 #endif
 };
+
+static void qx28_spi_init(void)
+{
+	spi_register_board_info(qx28_spi_board_info, ARRAY_SIZE(qx28_spi_board_info));
+}
 
 static struct mtd_partition qx28_nand_partitions[] = {
 	{
@@ -156,8 +261,8 @@ static void __init fixup_board(struct machine_desc *desc, struct tag *tags,
 
 static void __init qx28_device_init(void)
 {
-	/* Add qx28 special code */
-	i2c_device_init();
+	qx28_i2c_init();
+	qx28_spi_init();
 }
 
 static void __init qx28_init_machine(void)
@@ -179,6 +284,9 @@ static void __init qx28_init_machine(void)
 	mx28_register_nand_partitions(qx28_nand_partitions, ARRAY_SIZE(qx28_nand_partitions));
 	mx28_device_init();
 	qx28_device_init();
+
+	qx28_lon28_leds_init();
+	qx28_lon28_keys_init();
 }
 
 /* FIXME: "Freescale MX28" string is required for mfg updater tool */
