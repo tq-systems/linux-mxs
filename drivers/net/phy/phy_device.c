99264a93a66e7729602b94e69f26060b99b7cfd4
OpenWrt Developers Team <openwrt-devel@openwrt.org>
2013-03-10 22:04:06 +0100

    693-phy_attach_direct_cleanups

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -453,11 +453,11 @@ int phy_attach_direct(struct net_device *dev, struct phy_device *phydev,
 		      u32 flags, phy_interface_t interface)
 {
 	struct device *d = &phydev->dev;
+	int err;
 
 	/* Assume that if there is no driver, that it doesn't
 	 * exist, and we should use the genphy driver. */
 	if (NULL == d->driver) {
-		int err;
 		d->driver = &genphy_driver.driver;
 
 		err = d->driver->probe(d);
@@ -482,7 +482,11 @@ int phy_attach_direct(struct net_device *dev, struct phy_device *phydev,
 	/* Do initial configuration here, now that
 	 * we have certain key parameters
 	 * (dev_flags and interface) */
-	return phy_init_hw(phydev);
+	err = phy_init_hw(phydev);
+	if (err)
+		phy_detach(phydev);
+
+	return err;
 }
 EXPORT_SYMBOL(phy_attach_direct);
 
