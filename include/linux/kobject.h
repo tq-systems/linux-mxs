f75eb7853fe34a83e8daa96150ee89569678c970
OpenWrt Developers Team <openwrt-devel@openwrt.org>
2013-03-10 22:04:02 +0100

    223-kobject-add-broadcast_uevent

--- include/linux/kobject.h
+++ include/linux/kobject.h
@@ -30,6 +30,8 @@
 #define UEVENT_NUM_ENVP			32	/* number of env pointers */
 #define UEVENT_BUFFER_SIZE		2048	/* buffer for the variables */
 
+struct sk_buff;
+
 /* path to the userspace helper executed on an event */
 extern char uevent_helper[];
 
@@ -246,6 +248,10 @@ int add_uevent_var(struct kobj_uevent_env *env, const char *format, ...)
 
 int kobject_action_type(const char *buf, size_t count,
 			enum kobject_action *type);
+
+int broadcast_uevent(struct sk_buff *skb, __u32 pid, __u32 group,
+		     gfp_t allocation);
+
 #else
 static inline int kobject_uevent(struct kobject *kobj,
 				 enum kobject_action action)
@@ -262,6 +268,16 @@ static inline int add_uevent_var(struct kobj_uevent_env *env,
 static inline int kobject_action_type(const char *buf, size_t count,
 				      enum kobject_action *type)
 { return -EINVAL; }
+
+void kfree_skb(struct sk_buff *);
+
+static inline int broadcast_uevent(struct sk_buff *skb, __u32 pid, __u32 group,
+				   gfp_t allocation)
+{
+	kfree_skb(skb);
+	return 0;
+}
+
 #endif
 
 #endif /* _KOBJECT_H_ */
